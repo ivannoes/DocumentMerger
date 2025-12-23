360ff5b690ca6402683fdb1b99c1ee3c0702c3d9
ivannoes <ivannoes@hotmail.com>
2025-12-23 16:33:47 -0700

    Added UserDto and AddressDto classes with their respective creators.

---
+++ DocumentMerger/Dtos/AddressDto.cs
@@ -0,0 +1,16 @@
+﻿public class AddressDto : IDtoGeneric
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+}
+
+public class AddressDtoCreator : IDtoCreator
+{
+    public IDtoGeneric CreateDto(IDtoGeneric dtoData)
+    {
+        var data = dtoData as AddressDto;
+        return data;
+    }
+}
---
+++ DocumentMerger/Dtos/UserDto.cs
@@ -0,0 +1,15 @@
+﻿public class UserDto : IDtoGeneric
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
+
+public class UserDtoCreator : IDtoCreator
+{
+    public IDtoGeneric CreateDto(IDtoGeneric dtoData)
+    {
+        var data = dtoData as UserDto;
+        return data;
+    }
+}
