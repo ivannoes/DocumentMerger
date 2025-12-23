0d796b47cc90234f8652d391423d2c9c82a20710
ivannoes <ivannoes@hotmail.com>
2025-12-23 16:33:24 -0700

    Refactored the document merger architecture to introduce a generic DTO (IDtoGeneric) and creator (IDtoCreator) pattern.

---
+++ DocumentMerger/Dtos/DtoFactory.cs
@@ -0,0 +1,24 @@
+﻿public interface IDtoGeneric { }
+public interface IDtoCreator
+{
+    IDtoGeneric CreateDto(IDtoGeneric dtoData);
+}
+
+public class DtoCreator : IDtoCreator
+{
+    private readonly IDtoGeneric _dtoData;
+    public DtoCreator(IDtoGeneric dtoData)
+    {
+        _dtoData = dtoData;
+    }
+    public IDtoGeneric CreateDto(IDtoGeneric dtoData)
+    {
+        return dtoData switch
+        {
+            UserDto => new UserDtoCreator().CreateDto(dtoData),
+            AddressDto => new AddressDtoCreator().CreateDto(dtoData),
+            _ => throw new NotSupportedException("DTO type not supported"),
+        };
+    }
+}
+
