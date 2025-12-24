d8bb18d730ca84b2809d9c31606fd481a6081e69
ivannoes <ivannoes@hotmail.com>
2025-12-23 22:54:49 -0700

    Update mergeDocument to use UserDto with sample data
    Replaced the anonymous object previously passed to mergeDocument
    with a UserDto instance containing sample Id, Name, and Email
    properties. This change improves clarity and type safety.

--- DocumentMerger/Program.cs
+++ DocumentMerger/Program.cs
@@ -1,2 +1,8 @@
-﻿MergerAbstract merger = new PDFMerger();
-merger.mergeDocument("sample.pdf", new object() { });
+﻿var data = new UserDto
+{
+    Id = 1,
+    Name = "John Doe",
+    Email = ""
+};
+MergerAbstract merger = new PDFMerger();
+merger.mergeDocument("sample.pdf", data);
